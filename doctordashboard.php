d0b7ceb63e0de6fcf66ff264d8f80fbb15d4d57b
Pransi2003 <92987844+Pransi2003@users.noreply.github.com>
2023-03-17 03:21:50 +0530

    Add files via upload

---
+++ doctordashboard.php
@@ -0,0 +1,190 @@
+<?php
+session_start();
+include_once '../assets/conn/dbconnect.php';
+// include_once 'connection/server.php';
+if(!isset($_SESSION['doctorSession']))
+{
+header("Location: ../index.php");
+}
+$usersession = $_SESSION['doctorSession'];
+$res=mysqli_query($con,"SELECT * FROM doctor WHERE doctorId=".$usersession);
+$userRow=mysqli_fetch_array($res,MYSQLI_ASSOC);
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Welcome Dr <?php echo $userRow['doctorFirstName'];?> <?php echo $userRow['doctorLastName'];?></title>
+        <!-- Bootstrap Core CSS -->
+        <!-- <link href="assets/css/bootstrap.css" rel="stylesheet"> -->
+        <link href="assets/css/material.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="assets/css/sb-admin.css" rel="stylesheet">
+        <link href="assets/css/style.css" rel="stylesheet">
+        <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+        <!-- Custom Fonts -->
+    </head>
+    <body>
+        <div id="wrapper">
+
+            <!-- Navigation -->
+            <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="doctordashboard.php">Welcome Dr <?php echo $userRow['doctorFirstName'];?> <?php echo $userRow['doctorLastName'];?></a>
+                </div>
+                <!-- Top Menu Items -->
+                <ul class="nav navbar-right top-nav">
+                    
+                    
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $userRow['doctorFirstName']; ?> <?php echo $userRow['doctorLastName']; ?><b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="doctorprofile.php"><i class="fa fa-fw fa-user"></i> Profile</a>
+                            </li>
+                           
+                            <li class="divider"></li>
+                            <li>
+                                <a href="logout.php?logout"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav side-nav">
+                        <li class="active">
+                            <a href="doctordashboard.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="addschedule.php"><i class="fa fa-fw fa-table"></i> Doctor Schedule</a>
+                        </li>
+                        <li>
+                            <a href="patientlist.php"><i class="fa fa-fw fa-edit"></i> Patient List</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </nav>
+            <!-- navigation end -->
+
+            <div id="page-wrapper">
+                <div class="container-fluid">
+                    
+                    <!-- Page Heading -->
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h2 class="page-header">
+                            Dashboard
+                            </h2>
+                            
+                        </div>
+                    </div>
+                    <!-- Page Heading end-->
+
+                    <!-- panel start -->
+                    <div class="panel panel-primary filterable">
+                        <!-- Default panel contents -->
+                       <div class="panel-heading">
+                        <h3 class="panel-title">Appointment List</h3>
+                        
+                        </div>
+                        <div class="panel-body">
+                        <!-- Table -->
+                        <table class="table table-hover table-bordered">
+                            
+                            <?php 
+                            $res=mysqli_query($con,"SELECT a.*, b.*,c.*
+                                                    FROM patient a
+                                                    JOIN appointment b
+                                                    On a.icPatient = b.patientIc
+                                                    JOIN doctorschedule c
+                                                    On b.scheduleId=c.scheduleId
+                                                    Order By appId desc");
+                                  if (!$res) {
+                                    printf("Error: %s\n", mysqli_error($con));
+                                    exit();
+                                }
+                            while ($appointment=mysqli_fetch_array($res)) {
+                                
+                                if ($appointment['status']=='process') {
+                                    $status="danger";
+                                    $icon='remove';
+                                    $checked='';
+
+                                } else {
+                                    $status="success";
+                                    $icon='ok';
+                                    $checked = 'disabled';
+                                }
+
+                                
+                              
+                                
+                             
+                                
+
+                                
+
+                                echo "<tbody>";
+                                echo "<tr class='$status'>";
+                                    echo "<td>" . $appointment['patientIc'] . "</td>";
+                                    echo "<td>" . $appointment['patientLastName'] . "</td>";
+                                    echo "<td>" . $appointment['patientPhone'] . "</td>";
+                                    echo "<td>" . $appointment['patientEmail'] . "</td>";
+                                    echo "<td>" . $appointment['scheduleDay'] . "</td>";
+                                    echo "<td>" . $appointment['scheduleDate'] . "</td>";
+                                    echo "<td>" . $appointment['startTime'] . "</td>";
+                                    echo "<td>" . $appointment['endTime'] . "</td>";
+                                    echo "<td><span class='glyphicon glyphicon-".$icon."' aria-hidden='true'></span>".' '."". $appointment['status'] . "</td>";
+                                    echo "<form method='POST'>";
+                                    echo "<td class='text-center'><input type='checkbox' name='enable' id='enable' value='".$appointment['appId']."' onclick='chkit(".$appointment['appId'].",this.checked);' ".$checked."></td>";
+                                    echo "<td class='text-center'><a href='#' id='".$appointment['appId']."' class='delete'><span class='glyphicon glyphicon-trash' aria-hidden='true'></span></a>
+                            </td>";
+                               
+                            } 
+                                echo "</tr>";
+                           echo "</tbody>";
+                       echo "</table>";
+                       echo "<div class='panel panel-default'>";
+                       echo "<div class='col-md-offset-3 pull-right'>";
+                       echo "<button class='btn btn-primary' type='submit' value='Submit' name='submit'>Update</button>";
+                        echo "</div>";
+                        echo "</div>";
+                        ?>
+                    </div>
+                </div>
+         
+
+
+ 
+                </div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- /#page-wrapper -->
+        </div>
+        <!-- /#wrapper -->
+
+
+       
+        
+
+
+               
+
+    </body>
+</html>
